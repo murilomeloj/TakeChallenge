5eb8e7b19eceaf6997a89cec3616bb7bf36ed72b
murilomeloj <86209786+murilomeloj@users.noreply.github.com>
2022-08-13 14:42:24 -0300

    Delete api.js

--- API/api.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-const api = axios.create({
-    baseURL:"https://api.github.com/",
-});
-api.defaults.headers.common['Authorization']="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"
-
-module.exports = api;
